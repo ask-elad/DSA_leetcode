3ac8833847f7e5358d666eda485ac8164b246787
ask-elad <149039344+ask-elad@users.noreply.github.com>
2026-05-17 18:07:35 +0530

    Time: 5 ms (79.06%) | Memory: 44.7 MB (97.11%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int a = 0;
+        int b = 0;
+
+        int tot = 0;
+
+        while(a < g.size() && b < s.size()){
+
+            if(s[b] >= g[a]){
+                tot++;
+                a++;
+                b++;
+            }
+            else{
+                b++;
+            }
+        }
+
+        return tot;
+    }
+};
